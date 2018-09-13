a02ef448a329dbcf42f9663fa31408baa7444611
ooobsidian <obsidian_lrx@aliyun.com>
2018-09-14 00:08:40 +0800

    实现服务端多线程,完成群聊私聊方法

--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,11 @@
 import java.io.*;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @program: myChat
@@ -12,32 +16,61 @@ import java.util.Scanner;
 public class Server {
     private ServerSocket serverSocket;
 
+    private ExecutorService executorService;
+
+
+    private Map<String, PrintWriter> stringPrintWriterMap;
+
+
     public Server() {
         try {
             serverSocket = new ServerSocket(8080);
-            System.out.println("服务程序正在监听端口:" + serverSocket.getLocalPort());
+            stringPrintWriterMap = new HashMap<>();
+            executorService = Executors.newCachedThreadPool();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    // 将客户端的信息以map的形式存入集合中
+    private void addClient(String key, PrintWriter value) {
+        synchronized (this) {
+            stringPrintWriterMap.put(key, value);
+        }
+    }
+
+    // 将给点的输出流从共享集合中删除
+    private synchronized void remove(String key) {
+        stringPrintWriterMap.remove(key);
+        System.out.println("当前在线人数为: " + stringPrintWriterMap.size());
+    }
+
+    // 将给定的消息转发给所有客户端
+    private synchronized void sendMessageToAll(String message) {
+        for (PrintWriter out : stringPrintWriterMap.values()) {
+            out.println(message);
+        }
+    }
+
+    // 将给定的消息转发给私聊的客户端
+    private synchronized void sendMessageToSomeone(String name, String message) {
+        PrintWriter printWriter = stringPrintWriterMap.get(name);
+        if (printWriter != null) {
+            printWriter.println(message);
+        }
+    }
+
     public void run() {
         try {
-            System.out.println("等待客户端连接...");
-            Socket socket = serverSocket.accept();
-            System.out.println("客户端已连接");
-            InputStream inputStream = socket.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            OutputStream outputStream = socket.getOutputStream();
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, "UTF-8");
-            PrintWriter printWriter = new PrintWriter(outputStreamWriter, true);
-            Scanner scanner = new Scanner(System.in);
             while (true) {
-                System.out.println("客户端发来一条消息: " + bufferedReader.readLine());
-                printWriter.println(scanner.nextLine());
+                System.out.println("等待客户端连接...");
+                Socket socket = serverSocket.accept();
+                // 获取客户端的ip
+                InetAddress inetAddress = socket.getInetAddress();
+                System.out.println("客户端:" + inetAddress.getHostAddress() + "上线!");
+                System.out.println("当前聊天室在线人数为: " + stringPrintWriterMap.size());
+                executorService.execute(new ListenerClient(socket));
             }
-
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -45,14 +78,101 @@ public class Server {
         }
     }
 
+    class ListenerClient implements Runnable {
+        private Socket socket;
+        private String name;
+
+        public ListenerClient(Socket socket) {
+            this.socket = socket;
+        }
+
+        private String getName() throws Exception {
+            try {
+                // 服务端的输入读取客户端发送的昵称输出流
+                InputStream inputStream = socket.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                // 服务端将昵称验证结果通过自身输出流发送给客户端
+                OutputStream outputStream = socket.getOutputStream();
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, "UTF-8");
+                PrintWriter printWriter = new PrintWriter(outputStreamWriter, true);
+                // 读取客户端发来的昵称
+                while (true) {
+                    String nameString = bufferedReader.readLine();
+                    if ((nameString.trim().length() == 0) || stringPrintWriterMap.containsKey(nameString)) {
+                        printWriter.println("昵称不合法或是已经存在!");
+                    } else {
+                        printWriter.println("创建昵称成功!");
+                        return nameString;
+                    }
+                }
+            } catch (Exception e) {
+                throw e;
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                // 通过客户端的Socket获取客户端的输出流
+                OutputStream outputStream = socket.getOutputStream();
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, "UTF-8");
+                PrintWriter printWriter = new PrintWriter(outputStreamWriter, true);
+                //将客户昵称和其所发送的内容存在共享集合HashMap中
+                name = getName();
+                addClient(name, printWriter);
+                Thread.sleep(100);
+                sendMessageToAll("[系统通知]: " + name + "已上线!");
+                // 通过客户端的Socket获取输入流
+                // 读取客户端发送来的信息
+                InputStream inputStream = socket.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String msgString = null;
+
+                while ((msgString = bufferedReader.readLine()) != null) {
+                    // 检验是否为私聊(格式: @昵称:内容)
+                    if (msgString.startsWith("@")) {
+                        int index = msgString.indexOf(":");
+                        if (index >= 0) {
+                            // 获取昵称
+                            String theName = msgString.substring(1, index);
+                            String info = msgString.substring(index + 1, msgString.length());
+                            info = name + ": " + info;
+                            // 将私聊信息发送出去
+                            sendMessageToSomeone(theName, info);
+                            continue;
+                        }
+                    }
+                    // 遍历所有输出流,将该客户端发送的信息转发给所有客户端
+                    System.out.println(name + ": " + msgString);
+                    sendMessageToAll(name + ": " + msgString);
+                }
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                remove(name);
+                // 通知所有用户,某用户已经下线
+                sendMessageToAll("[系统通知]: " + name + "已下线!");
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
     public static void main(String... args) throws IOException {
         Server server = new Server();
         server.run();
-//        BufferedReader bufferedReader = new BufferedReader(
-//                new InputStreamReader(System.in));
-//
-//        System.out.print("请输入一系列文字，可包括空格：");
-//        String text = bufferedReader.readLine();
-//        System.out.println("请输入文字：" + text);
     }
 }
